4cc073ff8b0e319b99c652075a0b9d8557b75845
trgnam04 <trungnamtbcm123@gmail.com>
2024-09-26 13:57:24 +0700

    Commit - lab2_ex4 - done

---
+++ lab2/Core/Inc/lab2_ex4.h
@@ -0,0 +1,21 @@
+/*
+ * lab2_ex4.h
+ *
+ *  Created on: Sep 26, 2024
+ *      Author: ASUS
+ */
+
+#ifndef INC_LAB2_EX4_H_
+#define INC_LAB2_EX4_H_
+
+#include "main.h"
+
+void setTime(uint8_t Sec, uint8_t Min, uint8_t Hour);
+void blinkLed(void);
+void updateTime(void);
+void display_Led7seg(int counter);
+void display_Clock(void);
+void update7SEG(int index);
+void Clock_interface(void);
+
+#endif /* INC_LAB2_EX4_H_ */
